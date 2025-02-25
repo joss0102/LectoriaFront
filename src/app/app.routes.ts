67674884cb65ad10075171f0e10576561fcc50b3
joss.0102 <joserosellbonavina@gmail.com>
2025-02-25 22:06:36 +0100

    Mejora Nav-horizontal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,14 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './client/layout/layout/layout.component';
 
 import { InicioComponent } from './client/inicio/inicio/inicio.component';
+import { BibliotecaComponent } from './client/biblioteca/biblioteca/biblioteca.component';
 import { RouterModule } from '@angular/router';
 export const routes: Routes = [
     {
         path: "", component: LayoutComponent, children: [ // -> www.ejemplo.com
             // {path: "", component: InicioSesionComponent},
             {path: "inicio", component: InicioComponent}, // -> www.inicio.com
-            
-            // {path: "biblioteca", component: BibliotecaComponent},
+            {path: "biblioteca", component: BibliotecaComponent},
             
         ]
     }
--- src/app/client/header/nav-horizontal/nav-horizontal.component.ts
+++ src/app/client/header/nav-horizontal/nav-horizontal.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'app-nav-horizontal',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './nav-horizontal.component.html',
   styleUrl: './nav-horizontal.component.scss'
 })
 export class NavHorizontalComponent {
+  activeRoute: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.updateActiveRoute();
+    this.router.events.subscribe(() => {
+      this.updateActiveRoute();
+    });
+  }
+
+  updateActiveRoute() {
+    const currentPath = this.router.url.replace('/', '');
+    this.activeRoute = currentPath === 'inicio' || currentPath === 'biblioteca' ? currentPath : '';
+  }
 }

2f7c8d82371d116cae836ebd53a5d3a2dfcc1edf
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-03-27 14:35:50 +0100

    Rutas y moduleRutas eliminado + width nav horizontal fix

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+import { LayoutComponent } from './client/layout/layout.component';
+
+import { InicioSesionComponent } from './client/features/inicio-sesion/inicio-sesion/inicio-sesion.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { InicioComponent } from './client/inicio/inicio/inicio.component';
+import { BibliotecaComponent } from './client/biblioteca/biblioteca/biblioteca.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      // -> www.ejemplo.com
+      { path: '', component: InicioComponent },
+      { path: 'biblioteca', component: BibliotecaComponent },
+      { path: 'inicioSesion', component: InicioSesionComponent },
+    ],
+  },
+  { path: '**', component: PageNotFoundComponent },
+];

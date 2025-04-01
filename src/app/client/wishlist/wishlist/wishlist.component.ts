910f9d9db10b9123bac34bc9f80c31bc384743a0
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-04-01 16:40:14 +0200

    Componente wishlist + fix layout para responsive

--- src/app/client/wishlist/wishlist/wishlist.component.ts
+++ src/app/client/wishlist/wishlist/wishlist.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { DataBookComponent } from '../../library/data-book/data-book.component';
+import { CarouselWishComponent } from '../carousel-wish/carousel-wish.component';
 
 @Component({
   selector: 'app-wishlist',
   standalone: true,
-  imports: [],
+  imports: [DataBookComponent, CarouselWishComponent],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.scss',
 })

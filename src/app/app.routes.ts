97e4c1759dcefe3691ced50a3586e357bd4d15ed
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-04-03 21:53:56 +0200

    Settings + Qualifications elimienados y quitados.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,11 @@ import { LayoutComponent } from './client/layout/layout.component';
 import { HomeComponent } from './client/home/home/home.component';
 import { LoginComponent } from './client/features/login/login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 import { LibraryComponent } from './client/library/library/library.component';
 import { CurrentBookComponent } from './client/current-book/current-book/current-book.component';
 import { StadisticsComponent } from './client/stadistics/stadistics/stadistics.component';
-import { QualificationsComponent } from './client/qualifications/qualifications/qualifications.component';
 import { WishlistComponent } from './client/wishlist/wishlist/wishlist.component';
 import { UserComponent } from './client/user/user/user.component';
-import { SettingsComponent } from './client/settings/settings/settings.component';
 import { SearchBookComponent } from './client/features/search/search-book/search-book.component';
 import { SearchAuthorComponent } from './client/features/search/search-author/search-author.component';
 
@@ -26,9 +23,7 @@ export const routes: Routes = [
       { path: 'login', component: LoginComponent },
       { path: 'current-book', component: CurrentBookComponent },
       { path: 'statistics', component: StadisticsComponent },
-      { path: 'qualifications', component: QualificationsComponent },
       { path: 'wishlist', component: WishlistComponent },
-      { path: 'settings', component: SettingsComponent },
       { path: 'user', component: UserComponent },
       { path: 'searchAuthor', component: SearchAuthorComponent },
       { path: 'searchBook', component: SearchBookComponent },

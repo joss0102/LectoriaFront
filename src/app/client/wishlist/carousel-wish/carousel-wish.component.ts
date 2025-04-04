859f772661c9344e94a52447e0febf1a951e6334
joss.0102 <joserosellbonavina@gmail.com>
2025-04-04 18:35:45 +0200

    update Carrusel-WishList

--- src/app/client/wishlist/carousel-wish/carousel-wish.component.ts
+++ src/app/client/wishlist/carousel-wish/carousel-wish.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Book {
+  genres: string[];
+  genresColors: string[];
+  title: string;
+  description: string;
+  coverImage: string;
+  backgroundImage: string;
+}
 
 @Component({
   selector: 'app-carousel-wish',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel-wish.component.html',
   styleUrl: './carousel-wish.component.scss',
 })
-export class CarouselWishComponent {}
+export class CarouselWishComponent {
+  books: Book[] = [
+    {
+      genres: ['Trono de Cristal', 'Romance', 'Fantasía'],
+      genresColors: ['bg-info text-dark', 'bg-primary', 'bg-danger'],
+      title: 'Trono de Cristal',
+      description:
+        'En un reino donde la magia ha sido prohibida, una joven asesina es liberada de su prisión para competir contra los guerreros más letales del reino. Lo que comienza como una lucha por la libertad se convierte en una batalla por el destino de todo el mundo, mientras fuerzas oscuras conspiran en las sombras.',
+      coverImage:
+        '/libros/El principe cautivo/covers/El juego del principe.png',
+      backgroundImage: '/fondos/fondo_tog.jpg',
+    },
+    {
+      genres: ['Mi Alma es Tuya', 'Romance', 'Drama'],
+      genresColors: ['bg-info text-dark', 'bg-primary', 'bg-success'],
+      title: 'Mi Alma es Tuya',
+      description:
+        'Una historia de amor prohibido entre una humana y un ángel caído. Cuando el destino los une, descubren que su conexión va más allá de lo terrenal, desatando una guerra entre el cielo y el infierno. ¿Podrá su amor sobrevivir cuando todo el universo conspira en su contra?',
+      coverImage: '/libros/Alma/covers/Mi alma es tuya.png',
+      backgroundImage: '/libros/Alma/fondos/fondo1.jpg',
+    },
+  ];
+
+  currentIndex = 0;
+
+  prevSlide(): void {
+    this.currentIndex =
+      this.currentIndex > 0 ? this.currentIndex - 1 : this.books.length - 1;
+  }
+
+  nextSlide(): void {
+    this.currentIndex =
+      this.currentIndex < this.books.length - 1 ? this.currentIndex + 1 : 0;
+  }
+}

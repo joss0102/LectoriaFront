49fe943cd9d2651939f03fb75e9f3e3a1d480771
joss.0102 <joserosellbonavina@gmail.com>
2025-04-24 15:02:18 +0200

    modificacion search

--- src/app/client/features/search/search.component.spec.ts
+++ src/app/client/features/search/search.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SearchAuthorComponent } from './search-author.component';
+import { SearchComponent } from './search.component';
 
-describe('BuscadorAutoresComponent', () => {
-  let component: SearchAuthorComponent;
-  let fixture: ComponentFixture<SearchAuthorComponent>;
+describe('BuscadorComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SearchAuthorComponent],
+      imports: [SearchComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(SearchAuthorComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

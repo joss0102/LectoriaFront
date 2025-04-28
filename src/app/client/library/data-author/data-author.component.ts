98912664cfbe9fda665250a5e98138453f66baf3
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-04-28 17:56:24 +0200

    Nombre y capado a 5 autores

--- src/app/client/library/data-author/data-author.component.ts
+++ src/app/client/library/data-author/data-author.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorService } from '../../../core/services/call-api/author.service';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { Author } from '../../../core/models/call-api/author.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-data-author',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './data-author.component.html',
-  styleUrl: './data-author.component.scss'
+  styleUrl: './data-author.component.scss',
 })
-export class DataAuthorComponent {
+export class DataAuthorComponent implements OnInit {
+  constructor(
+    private authService: AuthService,
+    private authorService: AuthorService
+  ) {}
 
+  currentUser: string = '';
+  authors: Author[] = [];
+  topAuthors: Author[] = [];
+
+  ngOnInit(): void {
+    this.getCurrentUser();
+  }
+  getCurrentUser(): void {
+    const user = this.authService.currentUserValue;
+    if (user) {
+      this.currentUser = user.nickname;
+      this.finalizedBooks();
+    }
+  }
+  finalizedBooks(): void {
+    this.authorService.getAllAuthors().subscribe((authorsResponse) => {
+      this.authors = authorsResponse.data || [];
+      console.log(this.authors);
+      this.limitAuthors();
+    });
+  }
+  limitAuthors(): void {
+    this.topAuthors = this.authors.slice(0, 5);
+    console.log(this.topAuthors);
+  }
 }

d7f3d091a3d0c6dee40357d96066c2fd0e1a9aa4
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-05-06 02:03:03 +0200

    SemiCirculo no funcional

--- src/app/client/stadistics/stadistics/stadistics.component.ts
+++ src/app/client/stadistics/stadistics/stadistics.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { LinearGraphicComponent } from '../linear-graphic/linear-graphic.component';
+import { SemiCircleGaugeComponent } from '../semi-circle-gauge/semi-circle-gauge.component';
 
 @Component({
   selector: 'app-stadistics',
   standalone: true,
-  imports: [LinearGraphicComponent],
+  imports: [LinearGraphicComponent, SemiCircleGaugeComponent],
   templateUrl: './stadistics.component.html',
   styleUrl: './stadistics.component.scss',
 })

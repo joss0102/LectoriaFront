ebed7264f8121e8f949a93d40f3166d784fa3f2a
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-05-07 13:58:00 +0200

    Componente funcional

--- src/app/client/stadistics/mini-graphic/mini-graphic.component.ts
+++ src/app/client/stadistics/mini-graphic/mini-graphic.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { UserService } from '../../../core/services/call-api/user.service';
 
 @Component({
   selector: 'app-mini-graphic',
@@ -7,11 +9,35 @@ import { Component } from '@angular/core';
   templateUrl: './mini-graphic.component.html',
   styleUrls: ['./mini-graphic.component.scss'],
 })
-export class MiniGraphicComponent {
-  avg_pages_per_day: string = '73.02';
-  avg_reading_days_per_book: string = '7.6897';
+export class MiniGraphicComponent implements OnInit {
+  avg_pages_per_day: string = '0';
+  avg_reading_days_per_book: string = '0';
+
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.loadUserStats();
+  }
+
+  loadUserStats(): void {
+    const actualUser = this.authService.currentUserValue;
+    if (actualUser) {
+      this.userService.getUserStats(actualUser.nickname).subscribe({
+        next: (response) => {
+          this.avg_pages_per_day = (response.avg_pages_per_day || 0).toString();
+          this.avg_reading_days_per_book = (
+            response.avg_reading_days_per_book || 0
+          ).toString();
+        },
+      });
+    }
+  }
+
   getFormattedNumber(value: string): string {
     const num = parseFloat(value);
-    return num.toFixed(2);
+    return num.toFixed(0);
   }
 }

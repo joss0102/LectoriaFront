30c4fb9e64e3ccc7591ab5dc74cf112c688e4bc1
DavidFrontendDev <davidfrontenddev@gmail.com>
2025-05-19 15:53:30 +0200

    Quitando libreria IA

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { routes } from './app/app.routes';
-import 'deep-chat';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptorsFromDi()),
   ],
 }).catch((err) => console.error(err));
